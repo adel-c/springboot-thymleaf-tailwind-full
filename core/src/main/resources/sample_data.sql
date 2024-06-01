42358bb9d07d7bdf072d43507f96e79f37fb445d
Adel <576853-astran@users.noreply.gitlab.com>
2024-06-02 00:15:25 +0200

    add sample data

--- core/src/main/resources/sample_data.sql
+++
@@ -1,7 +0,0 @@
-delete from CUSTOMER;
-insert into CUSTOMER(id, name, email) values
-                                       ('1','customer1','a1@c.com'),
-                                       ('2','customer2','u2@c.com')
-
-
-
